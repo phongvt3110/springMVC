a3aa69c4ef34665fa35df491964be18ef8f6ae91
phongvt3110 <vtphong651043@gmail.com>
2017-09-26 17:15:46 +0700

    first commit

---
+++ src/main/java/com/ici/config/AppConfig.java
@@ -0,0 +1,33 @@
+package com.ici.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.ici")
+public class AppConfig extends WebMvcConfigurerAdapter{
+    @Bean
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/WEB-INF/views/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return internalResourceViewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/").setCachePeriod(31556926);
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer){
+        configurer.enable();
+    }
+}
---
+++ src/main/java/com/ici/controller/HomeController.java
@@ -0,0 +1,33 @@
+package com.ici.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/about")
+public class HomeController {
+    @GetMapping("/home")
+    public String home(Model model){
+        model.addAttribute("name","PhongVT");
+        return "home";
+    }
+
+    @RequestMapping(value = "welcome",method = RequestMethod.GET)
+    public String sayHello(ModelMap model){
+        model.addAttribute("greeting","Hello world from Sping Mvc 4");
+        return "welcome";
+    }
+    @RequestMapping(value = "/helloagain", method = RequestMethod.GET)
+    public String sayHelloAgain(ModelMap model){
+        model.addAttribute("greeting", "Hello World again from Spring MVC 4");
+        return "welcome";
+    }
+    @RequestMapping(value = "/frontend", method = RequestMethod.GET)
+    public String frondend(){
+        return "frontend/index";
+    }
+}
